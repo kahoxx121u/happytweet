fc0016672581bbf57e2c78e81984cb67cfbe99ea
kahoxx121u <smileyxoxokaho1212@gmail.com>
2023-03-13 00:43:37 +0900

    コメント機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,16 @@
 class TweetsController < ApplicationController
+
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user).order("created_at DESC")
   end
+
   def new
     @tweet = Tweet.new
   end
+
   def create
     Tweet.create(tweet_params)
   end
@@ -13,16 +19,34 @@ class TweetsController < ApplicationController
     tweet = Tweet.find(params[:id])
     tweet.destroy
   end
+
   def edit
-    @tweet = Tweet.find(params[:id])
   end
+
   def update
     tweet = Tweet.find(params[:id])
     tweet.update(tweet_params)
   end
+
+  def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
   private
+  
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
-  resources :tweets, only: [:index, :new, :create,:destroy,:edit,:update]
+  resources :tweets do
+    resources :comments, only: :create
+  end
+  resources :users, only: :show
 end
