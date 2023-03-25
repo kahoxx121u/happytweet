3ddaba7722cc6862d8f0b3905459d7c322a0acec
kahoxx121u <smileyxoxokaho1212@gmail.com>
2023-03-26 02:30:55 +0900

    画像表示機能

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,12 @@ class Tweet < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+  has_one_attached :image
+def self.search(search)
+  if search != ""
+    Tweet.where('text LIKE(?)', "%#{search}%")
+  else
+    Tweet.all
+  end
+end
 end
